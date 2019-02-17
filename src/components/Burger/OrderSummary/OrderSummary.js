d310f982f78be16e00ae5d51731b9091ff0a403c
omar fonseca <fon0@DharmaWheelRolling.com>
2019-02-16 16:58:24 -0800

    add a custom button component

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Aux from '../../../hoc/Aux'
+import Button from '../../UI/Button/Button'
 
 const orderSummary = (props) => {
     const ingredientSummary = Object.keys(props.ingredients).map(igKey => {
@@ -13,6 +14,8 @@ const orderSummary = (props) => {
             <ul>
                 {ingredientSummary}
             </ul>
+            <Button btnType="Danger" clicked={props.purchaseCancelled}>CANCEL</Button>
+            <Button btnType="Success" clicked={props.purchaseContinued}>CONTINUE</Button>
         </Aux>
     )   
 }
